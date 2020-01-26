3ea8d7f662c4d104a8f5e856e648d81d727abfd2
Wolfaholic <melissa.mackie@live.com>
2020-01-26 10:40:13 -0800

    finished 10th event listener

--- js/index.js
+++ js/index.js
@@ -56,22 +56,34 @@ element.style.transition = "all 0.3s";
  
 
 //Unique Event Listern 8:  Mousing over home container turns background blue
- const mouseOver = document.querySelector(".home");
+ const mouseOver = document.querySelector(".home .intro");
  mouseOver.addEventListener('mouseover', (event) => {
     event.target.style.background= "dodgerblue";
-   
-    setTimeout(function() {
-        event.target.style.background = "";
-      }, 500);
-    }, false);
+ });
+    // setTimeout(function() {
+    //     event.target.style.background = "";
+    //   }, 500);
+    // }, false);
  
 
 
-//Unique Event Listern 9:  
+//Unique Event Listern 9:  Mouse Leave
+const mouseLeave = document.querySelector(".home .intro"); 
+mouseLeave.addEventListener('mouseleave', (event) => {
+   event.target.style.background= "";
+});
  
  
- 
-//Unique Event Listern 10:  
+//Unique Event Listern 10:  resizing the window
 
+window.addEventListener("resize", () => {
+  const heightOutput = document.querySelector('#height');
+  const widthOutput = document.querySelector('#width');
+  function reportWindowSize() {
+    heightOutput.textContent = window.innerHeight;
+    widthOutput.textContent = window.innerWidth;
+  }
+  window.onresize = reportWindowSize;
+  })
 
 
